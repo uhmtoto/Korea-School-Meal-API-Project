66126a37831c50dc2941e0403e30b0a823786413
uhmtoto <uhmtotow@gmail.com>
2018-07-03 18:15:22 +0900

    다시 코드 깔끔하게
    아까 이상한 코드를 붙잡고 있었다;;

--- api.php
+++ api.php
@@ -1,30 +1,31 @@
 <?php
-    //include config, lib
+    //config, library
     include_once('./config/data.php');
     include_once('./config/dbconfig.php');
     include_once('./lib/snoopy/Snoopy.class.php');
 
-    //set variable
+    //set API key from GET method
     $_APIKEY = $_GET['apikey'];
 
     //SQL query
     $Query = "SELECT * FROM `api` WHERE `apikey`='$_APIKEY'";
     $res = mysql_query($Query, $conn);
     $res = mysql_fetch_array($res);
-    //print_r($res);
+
+    //set vars from $res
     $_SCname = $res['scname'];
     $_SCcode = $res['sccode'];
     $_SCloc = $res['scloc'];
     $_SCloc = $loc[$_SCloc];
     $_SCtype = $res['sctype'];
+    $_Mcode = $_GET['meal'];
+    $_Date = $_GET['date'];
     if($_SCtype == "초") $_SCtype = 2;
     if($_SCtype == "중") $_SCtype = 3;
     if($_SCtype == "고") $_SCtype = 4;
-    $_Mcode = $_GET['meal'];
     if($_Mcode == "조식") $_Mcode = 1;
     if($_Mcode == "중식") $_Mcode = 2;
     if($_Mcode == "석식") $_Mcode = 3;
-    $_Date = $_GET['date'];
 
     //parse with Snoopy class
     $URL = "https://stu.$_SCloc.go.kr/sts_sci_md01_001.do?schulCode=$_SCcode&schulCrseScCode=$_SCtype&schMmealScCode=$_Mcode&schYmd=$_Date";
@@ -40,18 +41,18 @@
     $cnt[0][1]=str_replace('<td class="textC">', '', $cnt[0][1]);
     $cnt[0][1]=str_replace('</td', '', $cnt[0][1]);
     $cnt = $cnt[0][1];
-    $fin = $fin[1];
-    $fin = $fin[date('w', strtotime($_Date))];
+    $fin = $fin[1][date('w', strtotime($_Date))];
     $fin = preg_replace('/[0-9@.]*/', '', $fin);
     $fin = str_replace('<br />', ',', $fin);
     $fin = substr_replace($fin, '', -1);
     if($fin=="") $fin = "오늘의 식단이 없습니다.";
 
-    //output
-    echo '<pre>{<br>';
-    echo '  "학교명":"'.$_SCname.'",<br>';
-    echo '  "일자":"'.$_Date.'",<br>';
-    echo '  "식단":"'.$fin.'",<br>';
-    echo '  "제공인원수":"'.$cnt.'"<br>';
-    echo '}</pre>';
+    //output with JSON
+    $json = json_encode(Array(
+        '학교명'=>$_SCname,
+        '일자'=>$_Date,
+        '식단'=>$fin,
+        '제공인원수'=>$cnt
+    ), JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+    echo($json);
 ?>
